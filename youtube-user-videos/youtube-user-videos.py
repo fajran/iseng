e72a5d7b2ade181f02e8c5f7c8123d17ceed2057
Fajran Iman Rusadi <fajran@gmail.com>
2008-06-30 22:56:26 +0200

    YouTube user videos

---
+++ youtube-user-videos/youtube-user-videos.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+#
+# Get video list of a user
+#
+# Requirement: Google Data API
+# License: Public Domain
+#
+# Usage: ./youtube-user-videos.py <username>
+#
+
+import sys
+from sys import stdout
+import gdata.youtube
+import gdata.youtube.service
+
+if len(sys.argv) < 2:
+	print "Usage: %s <username>" % sys.argv[0]
+	sys.exit(1)
+
+username = sys.argv[1]
+
+max = 50
+
+yt_service = gdata.youtube.service.YouTubeService()
+query = gdata.youtube.service.YouTubeVideoQuery()
+query.author = username
+query.max_results = max
+query.orderby = 'published'
+query.time = 'all_time'
+
+start = 1
+cnt = 0
+while True:
+	query.start_index = start
+	start += max
+
+	feed = yt_service.YouTubeQuery(query)
+	for item in feed.entry:
+		cnt += 1
+		data = (cnt,
+			item.media.player.url, 
+			item.media.duration.seconds,
+			item.media.title.text)
+		print "%d\t%s\t%s\t%s" % data
+
+	if len(feed.entry) < max:
+		break
