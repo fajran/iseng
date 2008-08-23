30812a6f4199fd5da6173234b987071d2b49287f
Fajran Iman Rusadi <fajran@gmail.com>
2008-08-23 14:08:20 +0700

    Add: mirror-debian-repository

---
+++ mirror-debian-repository/mirror-debian-repository.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+DIST=hoary
+SECTIONS="main restricted universe multiverse"
+ARCH=i386
+
+TARGET=ubuntu/
+BASE=http://old-releases.ubuntu.com/ubuntu/
+CUT=1
+
+
+for sect in $SECTIONS
+do
+	mkdir -p $TARGET/dists/$DIST/$sect/binary-$ARCH/
+	wget -c -O $TARGET/dists/$DIST/$sect/binary-$ARCH/Packages.gz $BASE/dists/$DIST/$sect/binary-$ARCH/Packages.gz
+	gunzip -c $TARGET/dists/$DIST/$sect/binary-$ARCH/Packages.gz > $TARGET/dists/$DIST/$sect/binary-$ARCH/Packages
+done
+
+LIST=$TARGET/.list.txt
+[ -f $LIST ] && rm -f $LIST
+
+for sect in $SECTIONS
+do
+	grep ^Filename: $TARGET/dists/$DIST/$sect/binary-$ARCH/Packages | cut -d' ' -f 2 >> $LIST
+done
+
+cat $LIST | while read url
+do
+	wget -r -nH --cut-dirs=$CUT -P $TARGET -c $BASE/$url
+done
+
