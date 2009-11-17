fec8402424a0912387a2978c72997dfa780cda2e
Fajran Iman Rusadi <fajran@gmail.com>
2009-11-17 06:44:34 +0100

    Add a brief documentation.

--- download-debian-dists/download-dists.sh
+++ download-debian-dists/download-dists.sh
@@ -1,5 +1,35 @@
 #!/bin/sh
 
+# Download Debian Repository's Package Index
+
+# Author: Fajran Iman Rusadi
+# License: Public Domain
+
+# Usage:
+#
+#     ./download-dists [repo base url] [dists] [sections] [archs] [target]
+#
+#     - repo base url is the base URL of a Debian repository
+#       i.e. http://ftp.debian.org/debian/
+#     - dists is a comma separated distribution/release names
+#       i.e. lenny
+#     - sections is a list of repository sections, separated by a comma
+#       i.e. main,contrib,non-free
+#     - archs is a list of architectures, separated by a comma
+#       i.e. i386,amd64
+#     - target is the target download directory
+#       i.e. /home/you/debian/
+#
+# Example:
+#
+#     ./download-dists \
+#          http://kambing.ui.ac.id/ubuntu/ \
+#          karmic,karmic-updates,karmic-security \
+#          main,restricted,universe,multiverse
+#          i386 \
+#          ubuntu/
+#
+
 BASE=$1
 DISTS=$2
 SECTIONS=$3
